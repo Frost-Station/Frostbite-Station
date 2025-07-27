2beba7dbcc4d8babe75e4e2198727eb0a51b39d7
beepbeep <112143520+and831@users.noreply.github.com>
2025-07-27 11:28:43 -0600

    Update CCVars.Atmos.cs

--- Content.Shared/CCVar/CCVars.Atmos.cs
+++ Content.Shared/CCVar/CCVars.Atmos.cs
@@ -21,14 +21,14 @@ public sealed partial class CCVars
     ///     A direct multiplier on how violent space wind is.
     /// </summary>
     public static readonly CVarDef<float> SpaceWindStrengthMultiplier =
-        CVarDef.Create("atmos.space_wind_strength_multiplier", 1f, CVar.SERVERONLY);
+        CVarDef.Create("atmos.space_wind_strength_multiplier", 0f, CVar.SERVERONLY);
 
     /// <summary>
     ///     The maximum Force (in Newtons) that may be applied to an object by atmospheric pressure differences.
     ///     Useful to prevent clipping through objects.
     /// </summary>
     public static readonly CVarDef<float> SpaceWindMaxForce =
-        CVarDef.Create("atmos.space_wind_max_force", 200f, CVar.SERVERONLY);
+        CVarDef.Create("atmos.space_wind_max_force", 0f, CVar.SERVERONLY);
 
     /// <summary>
     ///     The maximum angular velocity that space wind can spin objects at while throwing them. This one is mostly for fun.
@@ -40,7 +40,7 @@ public sealed partial class CCVars
     ///     The amount of time (in seconds) for space wind to knock down a player character if they are subjected to space wind.
     /// </summary>
     public static readonly CVarDef<float> SpaceWindKnockdownTime =
-        CVarDef.Create("atmos.space_wind_knockdown_time", 0.75f, CVar.SERVERONLY);
+        CVarDef.Create("atmos.space_wind_knockdown_time", 0f, CVar.SERVERONLY);
 
     /// <summary>
     ///     Whether monstermos tile equalization is enabled.
